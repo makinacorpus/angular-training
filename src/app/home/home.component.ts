5315799cab6638ac0b37507671fc7f034e2abd75
Eric BREHAULT <ebrehault@gmail.com>
2017-02-21 17:29:52 +0100

    Use ngClass

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,9 +10,9 @@ export class HomeComponent implements OnInit {
   @Input() message: string;
 
   private pokemons:any[] = [
-    {id: 1, name: 'Balbusar', pv: 75},
-    {id: 2, name: 'Carapuce', pv: 31},
-    {id: 2, name: 'Salamesh', pv: 18},
+    {id: 1, name: 'Balbusar', pv: 75, size: 150, type: 'grass'},
+    {id: 2, name: 'Carapuce', pv: 31, size: 45, type: 'electric'},
+    {id: 2, name: 'Salamesh', pv: 18, size: 75, type: 'fire'},
   ];
 
   constructor() { }
@@ -27,5 +27,16 @@ export class HomeComponent implements OnInit {
   isStronger(pokemon:any) {
     let max_pv = Math.max(...this.pokemons.map(pok => pok.pv));
     return (pokemon.pv == max_pv)
+  }
+
+  getClasses(pokemon:any) {
+    return {
+      grass: pokemon.type == 'grass',
+      electric: pokemon.type == 'electric',
+      fire: pokemon.type == 'fire',
+      small: pokemon.size < 50,
+      medium: pokemon.size < 100 && pokemon.size >= 50,
+      big: pokemon.size >= 100
+    }
   }
 }

3c216e2e53f2250d5d604e99fd640aec768fa114
Eric BREHAULT <ebrehault@gmail.com>
2017-02-22 19:30:31 +0100

    Put API result in cache

--- src/app/api.ts
+++ src/app/api.ts
@@ -6,13 +6,22 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class APIService {
 
+  private cache:any[];
+
   constructor(private http: Http) {}
 
   listAll(): Observable<any[]> {
-    return this.http.get('http://pokeapi.co/api/v2/pokemon/')
-    .map(res => {
-      return res.json().results;
-    });
+    if(!this.cache) {
+      return this.http.get('http://pokeapi.co/api/v2/pokemon/')
+      .map(res => {
+        this.cache = res.json().results;
+        return this.cache;
+      });
+    } else {
+      return new Observable(observer => {
+        observer.next(this.cache);
+      });
+    }
   }
 
   get(id:string): Observable<any> {

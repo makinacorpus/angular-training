d7b02efba2cac3d5c6d36b4338677aaef9f7ea10
Eric BREHAULT <ebrehault@gmail.com>
2017-02-22 17:32:40 +0100

    Add spinner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,14 @@ import { Http } from '@angular/http';
 export class HomeComponent implements OnInit {
 
   private pokemons:any[] = [];
+  private isLoading:boolean = true;
 
   constructor(private http: Http) { }
 
   ngOnInit() {
     this.http.get('http://pokeapi.co/api/v2/pokemon/')
     .subscribe(res => {
+      this.isLoading = false;
       this.pokemons = res.json().results.map(item => {
         return {
           id: item.url.split('/').reverse()[1],

c1bd0305a0b33c390894d459ca378ba52054a728
Eric BREHAULT <ebrehault@gmail.com>
2017-02-22 12:34:33 +0100

    Add the About page and its route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,

9d771856bb09642bd34aa146d2bc4ed2ed255a6f
Eric BREHAULT <ebrehault@gmail.com>
2017-02-22 14:05:46 +0100

    Move pokemons from Home to config file

---
+++ src/app/config.ts
@@ -0,0 +1,5 @@
+export const POKEMONS:any[] = [
+  {id: 1, name: 'Balbusar', pv: 75, size: 150, type: 'grass'},
+  {id: 2, name: 'Carapuce', pv: 31, size: 45, type: 'electric'},
+  {id: 3, name: 'Salamesh', pv: 18, size: 75, type: 'fire'},
+]

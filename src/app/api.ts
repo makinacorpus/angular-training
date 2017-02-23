faa6d6c7511c7c2921d11c763de11e401ce9d31e
Eric BREHAULT <ebrehault@gmail.com>
2017-02-23 16:08:55 +0100

    Manage a global cache for all calls

--- src/app/api.ts
+++ src/app/api.ts
@@ -6,33 +6,41 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class APIService {
 
-  private cache:any[];
+  private cache:any = {};
+  private obs: Observable<any>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+     this.obs = new Observable(observer => {
+       observer.next(this.cache);
+     });
+  }
 
   listAll(): Observable<any[]> {
-    if(!this.cache) {
+    if(!this.cache.all) {
       return this.http.get<any>('http://pokeapi.co/api/v2/pokemon/')
       .map(res => {
-        this.cache = res.results;
-        return this.cache;
+        this.cache.all = res.results;
+        return this.cache.all;
       });
     } else {
-      return new Observable(observer => {
-        observer.next(this.cache);
-      });
+      return this.obs.map(cache => cache.all);
     }
   }
 
   get(id:string): Observable<any> {
-    return this.http.get<any>('http://pokeapi.co/api/v2/pokemon/' + id)
-    .map(pokemon => {
-      if(pokemon.detail) {
-        return {error: pokemon.detail};
-      } else {
-        pokemon.type = pokemon.types.filter(type => type.slot==1)[0].type.name;
-        return pokemon;
-      }
-    });
+    if(!this.cache[id]) {
+      return this.http.get<any>('http://pokeapi.co/api/v2/pokemon/' + id)
+      .map(pokemon => {
+        if(pokemon.detail) {
+          return {error: pokemon.detail};
+        } else {
+          pokemon.type = pokemon.types.filter(type => type.slot==1)[0].type.name;
+          this.cache[id] = pokemon;
+          return pokemon;
+        }
+      });
+    } else {
+      return this.obs.map(cache => cache[id]);
+    }
   }
 }
